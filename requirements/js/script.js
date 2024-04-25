e61a26d5027d3dd91f26bf1292b8f6bda7021a94
marin3-x <amit.sharma8990x@gmail.com>
2024-04-25 13:35:54 +0530

    first commit

---
+++ requirements/js/script.js
@@ -0,0 +1,58 @@
+// function showAlert() {
+//     alert("the website is under constraction");
+// }
+// var typed = new Typed(".auto-type", {
+//     strings: ["web dev.", "web3 dev.", "app dev."],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     loop: true
+// });
+// let curTheme = "light";
+// let themeBtn = document.querySelector("#themeBtn");
+// let a = document.querySelectorAll('.nav-link');
+// console.log(a);
+// themeBtn.addEventListener("click", () => {
+//     if (curTheme === "light") {
+//         curTheme = "dark";
+//         document.querySelector("body").setAttribute("class", "dark");
+//         a.classList.add(".a-dark");
+//     } else { curTheme = "light";
+//     document.querySelector("body").setAttribute("class", "light");
+//     a.classList.add(".a-dark"); }
+// })
+
+
+function showAlert() {
+    alert("the website is under construction");
+}
+
+var typed = new Typed(".auto-type", {
+    strings: ["web dev.", "web3 dev.", "app dev."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
+
+let curTheme = "light";
+let themeBtn = document.querySelector("#themeBtn");
+let a = document.querySelectorAll('.nav-link');
+
+themeBtn.addEventListener("click", () => {
+    if (curTheme === "light") {
+        curTheme = "dark";
+        document.querySelector("body").setAttribute("class", "dark");
+        for (let link of a) {
+            link.classList.add("a-dark");
+        }
+        document.querySelector(".myAbout").classList.add("myAbout-dark");
+        document.querySelector("#themeBtn").setAttribute("class", "ri-sun-line");
+    } else {
+        curTheme = "light";
+        document.querySelector("body").setAttribute("class", "light");
+        for (let link of a) {
+            link.classList.remove("a-dark");
+            document.querySelector(".myAbout").classList.remove("myAbout-dark");
+            document.querySelector("#themeBtn").setAttribute("class", "ri-moon-line");
+        }
+    }
+});
