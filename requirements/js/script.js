3774fa891dffe5c9cf20b5e9bfe2f2f080f76d26
marin3-x <amit.sharma8990x@gmail.com>
2024-05-08 14:08:47 +0530

    improving the styling

--- requirements/js/script.js
+++ requirements/js/script.js
@@ -33,3 +33,42 @@ themeBtn.addEventListener("click", () => {
         }
     }
 });
+let loaderAnimation = (() => {
+    let tl = gsap.timeline();
+
+    tl.from("#loader h3", {
+        x: 40,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.1
+    })
+    .to("#loader h3", {
+        opacity: 0,
+        x: -10,
+        duration: 1,
+        stagger: 0.1
+    })
+    .to("#loader", {
+        opacity: 0,
+        duration: 0.1,
+        display: "none"
+    });
+
+    return tl;
+});let pageAnimation = (() => {
+    let tl = gsap.timeline();
+
+    tl.from(".logo", { duration: 0.5, opacity: 0, y: -50, ease: "power4.out" })
+    tl.from("#themeBtn", { duration: 0.8, opacity: 0, y: -50, stagger: 0.2, ease: "power4.out" }, "-=0.5")
+      tl.from(".nav-link", { duration: 0.8, opacity: 0, y: -50, stagger: 0.2, ease: "power4.out" }, "-=0.5")
+      tl.from(".ri-menu-3-fill", { duration: 0.8, opacity: 0, x: 50, ease: "power4.out" }, "-=0.5")
+      tl.from(".left", { duration: 0.8, opacity: 0, x: -50, ease: "power4.out" }, "-=0.5")
+      tl.from(".right", { duration: 0.8, opacity: 0, x: 50, ease: "power4.out" }, "-=0.5")
+      tl.from(".social a", { duration: 0.8, opacity: 0, y: 50, stagger: 0.2, ease: "power4.out" }, "-=0.5");
+
+    return tl;
+});
+
+let masterTimeline = gsap.timeline();
+masterTimeline.add(loaderAnimation()).add(pageAnimation());
+
